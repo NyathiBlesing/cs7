f14d7519def33707a8a22b1692126ce9238fe04c
Blessing Nyathi <blessingmdluli04@gmail.com>
2025-07-21 13:10:48 +0200

    Lab0
    This is my Lab 0, used to verify that my coding environment is set up correctly. It contains a simple Python program that evaluates to 2024 — the year I'm taking this course.

---
+++ lab0.py
@@ -0,0 +1,12 @@
+#Replace the email below with your own email. Surround it by double quotes too as shown below.
+email = "blessingmdluli04@gmail.com" 
+
+def twenty_twenty_four():
+    """Come up with the most creative expression that evaluates to 2024,
+    using ONLY numbers and the +, *, and - operators. Use ALL three of 
+    these operators ONLY to get full credit. Do NOT use division.
+
+    >>> twenty_twenty_four()
+    2024
+    """
+    return 25 * 4 + 1950 - 26
